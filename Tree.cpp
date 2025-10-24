2678f47e6322f4f10394f79145399a8ff2fedc0b
OHHHHHH <2859611604@qq.com>
2025-10-25 01:10:58 +0800

    Complete recover binary tree

---
+++ Tree.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+
+typedef struct BTNode
+{
+    char data;
+    BTNode* left;
+    BTNode* right;
+}BTNode;
+
+
+void DispBTree(BTNode *b)
+{
+    if (b != NULL)
+    {
+        printf("%c", b->data);
+        if (b->left != NULL || b->right != NULL)
+        {
+            printf("(");
+            DispBTree(b->left);
+            printf(",");
+            DispBTree(b->right);
+            printf(")");
+        }
+
+    }
+}
+
+BTNode* CreateBT(char *pre, char *in, int n)
+{
+    BTNode* head;
+    BTNode* help;
+    head = (BTNode*)malloc(sizeof(BTNode));
+
+    head->data = pre[0];
+    head->left = head->right = NULL;
+
+    //中序中找到根节点
+    int index = 0;
+    while(in[index] != head->data)
+    {
+        index++;
+    }
+    //左子树大小
+    int len_left = index;
+    //右子树大小
+    int len_right = n - len_left - 1;
+
+    if (len_left > 0)
+    {
+        //创建左子树
+        head->left = CreateBT(pre + 1, in, len_left);
+    }
+    if (len_right > 0)
+    {
+        //创建右子树
+        head->right = CreateBT(pre + len_left + 1, in + len_left + 1, len_right);
+    }
+
+    //返回根节点
+    return head;
+}
+
+
+
+int main()
+{
+    char pre[100];
+    char in[100];
+    BTNode* tree;
+    gets(pre);
+    gets(in);
+    int len_in = strlen(in);
+
+    tree = CreateBT(pre, in, len_in);
+
+    DispBTree(tree);
+
+    return 0;
+}
+
