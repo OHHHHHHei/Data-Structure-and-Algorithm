66cd344c1c6776ae9caad64ccffafd4bebc3561c
OHHHHHH <2859611604@qq.com>
2025-10-25 01:17:35 +0800

    Update

--- Tree.cpp
+++ Tree.cpp
@@ -30,36 +30,45 @@ void DispBTree(BTNode *b)
 
 BTNode* CreateBT(char *pre, char *in, int n)
 {
+    // 1. 创建当前根节点
     BTNode* head;
-    BTNode* help;
     head = (BTNode*)malloc(sizeof(BTNode));
 
+    // 2. 先序第一个元素就是根节点的值
     head->data = pre[0];
     head->left = head->right = NULL;
 
-    //中序中找到根节点
+    // 3. 在中序遍历中找到根节点的位置
+    //    这样可以确定左右子树的范围
     int index = 0;
     while(in[index] != head->data)
     {
-        index++;
+        index++;  // index 最终指向根节点在中序中的位置
     }
-    //左子树大小
-    int len_left = index;
-    //右子树大小
-    int len_right = n - len_left - 1;
+    
+    // 4. 计算左右子树的大小
+    int len_left = index;              // 左子树节点数
+    int len_right = n - len_left - 1;  // 右子树节点数 (总数 - 左子树 - 根节点)
 
+    // 5. 递归创建左子树
     if (len_left > 0)
     {
-        //创建左子树
+        // pre + 1: 跳过根节点,从左子树的先序起点开始
+        // in: 中序从头开始就是左子树
+        // len_left: 左子树的长度
         head->left = CreateBT(pre + 1, in, len_left);
     }
+    
+    // 6. 递归创建右子树
     if (len_right > 0)
     {
-        //创建右子树
+        // pre + len_left + 1: 跳过根节点(+1)和整个左子树(+len_left)
+        // in + len_left + 1: 跳过左子树和根节点
+        // len_right: 右子树的长度
         head->right = CreateBT(pre + len_left + 1, in + len_left + 1, len_right);
     }
 
-    //返回根节点
+    // 7. 返回构建好的子树根节点
     return head;
 }
 
