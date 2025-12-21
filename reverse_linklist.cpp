0cbc21e30ebf8d719011b48f49166e363c7df33b
OHHHHHH <2859611604@qq.com>
2025-12-21 18:07:07 +0800

    实现链表结构和基本创建功能

--- reverse_linklist.cpp
+++ reverse_linklist.cpp
@@ -1,2 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+struct node
+{
+    char data;
+    node *next;
+};
+
+void reverseList(node *head, node *first)
+{
+    
+}
+
+int main()
+{
+    struct node *head = NULL , *tail = NULL, *p;
+    char data;
+    int length;
+    printf("链表长度为：");
+    scanf("%d", &length);
+
+    printf("输入元素");
+    for (int i = 0; i < length; i++)
+    {
+        scanf("%c ", &data);
+        p = (struct node *)malloc(sizeof(node));
+        p->data = data;
+        p->next = NULL;
+        
+        if (head == NULL)
+        {
+            head = p;
+            tail = p;
+        }
+        else
+        {
+            tail->next = p;
+            tail = p;
+        }
+    }
+
+
+
+}
